2904b7efdd364c5a72996a5763e08c8ddc8a5955
KasiaJakubowska <katarzyna.jakubowska1998@gmail.com>
2019-04-30 20:56:34 +0200

    New

---
+++ .history/main_20190426132041.js
@@ -0,0 +1,37 @@
+const slideList = [{
+ img: "images/img1.jpg",
+ text: 'Pierwszy tekst'
+},
+{
+ img: "images/img2.jpg",
+ text: 'Drugi tekst'
+},
+{
+ img: "images/img3.jpg",
+ text: 'Trzeci tekst'
+}];
+
+const image = document.querySelector('img.slider');
+const h1 = document.querySelector('h1.slider');
+const dots = document.querySelector
+// Interfejs
+const time = 3000;
+let active = 0;
+
+// Implementacje
+
+const changeDot = () => {
+
+}
+
+const changeSlide = () => {
+ active++;
+ if (active === slideList.length) {
+  active = 0;
+ }
+ image.src = slideList[active].img;
+ h1.textContent = slideList[active].text;
+changeDot()
+}
+setInterval(changeSlide, time)
+
